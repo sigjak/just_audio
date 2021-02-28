1a5eb91eee696434d0c8fb488016fd38a734ef10
sigjak <sigjak@gmail.com>
2021-02-28 19:32:41 +0000

    init

---
+++ lib/screens/player.dart
@@ -0,0 +1,77 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:just_audio/just_audio.dart';
+import './commons/player_buttons.dart';
+import './commons/slider.dart';
+import '../screens/helpers/meta.dart';
+
+class Player extends StatefulWidget {
+  @override
+  _PlayerState createState() => _PlayerState();
+}
+
+class _PlayerState extends State<Player> {
+  AudioPlayer _audioPlayer;
+  List<String> audioFiles;
+  List<AudioSource> myList = [
+    AudioSource.uri(Uri.parse("asset:///audio/book/guard.mp3"),
+        tag: AudioMetadata(album: 'Grisham: The Guardians')),
+  ];
+  @override
+  void initState() {
+    super.initState();
+    _audioPlayer = AudioPlayer();
+
+    _audioPlayer
+        .setAudioSource(ConcatenatingAudioSource(children: myList))
+        .catchError((error) {
+      // catch load errors: 404, invalid url ...
+      print("An error occured $error");
+    });
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    super.dispose();
+  }
+
+  Future<List<String>> getAssetFiles() async {
+    final manifestContent = await rootBundle.loadString('AssetManifest.json');
+    final Map<String, dynamic> manifestMap = jsonDecode(manifestContent);
+    List<String> myfiles = [];
+    List<String> gg =
+        manifestMap.keys.where((key) => key.contains('.mp3')).toList();
+    print(gg);
+    gg.forEach((element) {
+      String temp = element.replaceAll('%20', ' ');
+      myfiles.add(temp);
+    });
+    return myfiles;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          StreamBuilder<SequenceState>(
+              stream: _audioPlayer.sequenceStateStream,
+              builder: (_, snapshot) {
+                final state = snapshot.data;
+                // if (state?.sequence?.isEmpty ?? true) return SizedBox();
+                return state != null
+                    ? Text(state.sequence[0].tag.album)
+                    : Text('');
+              }),
+          PlayerButtons(_audioPlayer),
+          SliderBar(_audioPlayer),
+        ],
+      ),
+    ));
+  }
+}
